2234b828f85379f4c9fa482c511f7590b87c6d48
superkid200 <minh306@outlook.com.vn>
2018-07-08 14:46:06 +0700

    Add code

---
+++ SplitViewNavigation/SplitViewNavigation/NavigationItem.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SplitViewNavigation
+{
+    public sealed partial class NavigationItem : UserControl
+    {
+        private IconElement iconElement;
+        public IconElement Icon { get { return iconElement; } set { if (MainItem.Children.Count == 2) { MainItem.Children.RemoveAt(0); } MainItem.Children.Insert(0, value); iconElement = value; } }
+
+        private string text;      
+        public string Text { get { return text; } set { TextBlock.Text = value; text = value; } }
+        public Type NavigateTo { get; set; }
+        public NavigationItem()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ SplitViewNavigation/SplitViewNavigation/SplitView.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SplitViewNavigation
+{
+    public sealed partial class SplitView : UserControl
+    {
+        private bool isPaneOpen;
+        public bool IsPaneOpen { get { return isPaneOpen; } set { Splitter.IsPaneOpen = value; isPaneOpen = value; } }
+
+        private IList<NavigationItem> topItems = new List<NavigationItem>();
+        public IList<NavigationItem> TopItems { get { return topItems; } set { foreach (var item in value) { TopListView.Items.Add(item); } topItems = value; } }
+
+        private IList<NavigationItem> bottomItems = new List<NavigationItem>();
+        public IList<NavigationItem> BottomItems { get { return bottomItems; } set { foreach (var item in value) { BottomListView.Items.Add(item); } bottomItems = value; } }
+        public SplitView()
+        {
+            this.InitializeComponent();
+            frame = rootFrame;
+        }
+        private Frame frame;
+        public Frame Frame { get { return Frame; } }
+
+        private void TopListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListView mainSender = sender as ListView;
+            if(mainSender.SelectedIndex == -1)
+            {
+                return;
+            }
+            BottomListView.SelectedIndex = -1;
+            Type page = ((NavigationItem)mainSender.SelectedItem).NavigateTo;
+            Frame.Navigate(page);
+        }
+
+        private void BottomListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListView mainSender = sender as ListView;
+            if (mainSender.SelectedIndex == -1)
+            {
+                return;
+            }
+            TopListView.SelectedIndex = -1;
+            Type page = ((NavigationItem)mainSender.SelectedItem).NavigateTo;
+            Frame.Navigate(page);
+        }
+    }
+}
